87e16eaccd70d4f543ee64c1aebb9f5ac1798992
Murilo Ribeiro <murilo.dsribeiro@gmail.com>
2019-10-23 15:33:03 -0300

    new commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import './App.css';
 import AnterosButton from '../src/components/AnterosButton'
 
 function App() {
   return (
-    <div style={{flex:1,alignItems:'center', justifyContent: 'center'}}>
+    <div style={{alignItems:'center', justifyContent: 'center'}}>
       <AnterosButton
       style={{width:120,height:40,margin:20}}
-      oval={true} warning={true}>
+      oval={false} danger={true}>
         <p>Olá, tudo bem?</p>
       </AnterosButton>
-      
     </div>
   );
 }
